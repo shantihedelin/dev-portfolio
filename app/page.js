4dd88e564b46e9d9b4b72005813e8d49e41923f3
shantihedelin <145372686+shantihedelin@users.noreply.github.com>
2024-08-07 23:50:37 +0200

    did some responsive shit

--- app/page.js
+++ app/page.js
@@ -33,7 +33,7 @@ export default function Home() {
   return (
     <main className="text-white">
       <Nav />
-      <div className="m-12 flex md:m-8">
+      <div className="hidden m-12 md:flex md:m-8">
         <div className="flex items-center  flex-col md:flex-row md:m-4 md:space-x-8">
           <div className="relative h-96 my-4 w-full">
             <Image
@@ -43,19 +43,19 @@ export default function Home() {
               alt="Headshot of Shanti Hedelin"
             />
           </div>
-          <div className="flex justify-center flex-col items-center mb-2 md:text-center md:space-y-12 bg-purple-500 p-8 rounded-xl shadow-2xl bg-opacity-50" >
+          <div className="flex justify-center flex-col items-center mb-2 md:text-center md:space-y-12 bg-purple-500 p-8 rounded-xl shadow-2xl bg-opacity-40">
             <h2 className="text-5xl m-0">
               Hello, my name is Shanti Hedelin, i'm a{" "}
               <span className="fullstack">frontend developer</span>
             </h2>
             <p className="pt-20">
-              Welcome to my page. I am a dedicated frontend developer with significant experience in
-              building high-performance and secure websites, and I also have
-              strong backend development skills. I work well in teams and strive
-              to deliver smart and intuitive code. I'm detail-oriented and
-              prioritize creating an optimal user experience. My focus and
-              dedication ensure that I always aim to achieve the best possible
-              results for every project I work on.
+              Welcome to my page. I am a dedicated frontend developer with
+              significant experience in building high-performance and secure
+              websites, and I also have strong backend development skills. I
+              work well in teams and strive to deliver smart and intuitive code.
+              I'm detail-oriented and prioritize creating an optimal user
+              experience. My focus and dedication ensure that I always aim to
+              achieve the best possible results for every project I work on.
             </p>
             <button className="bg-transparent bg-white w-64 border p-4 shadow-xl rounded-3xl hover:cursor-pointer hover:w-96 transistion duration-300 ease-in-out mt-8">
               View Resume
@@ -63,8 +63,43 @@ export default function Home() {
           </div>
         </div>
       </div>
+
+      <div className="m-12 flex md:hidden">
+        <div className="flex items-center  flex-col md:flex-row md:m-4 md:space-x-8">
+          <div className="flex justify-center flex-col items-center mb-2 md:text-center md:space-y-12">
+            <h2 className="text-5xl m-0">
+              Hello, my name is Shanti Hedelin, i'm a{" "}
+              <span className="fullstack">frontend developer</span>
+            </h2>          <div className="relative h-96 my-4 w-full">
+            <Image
+              src={headshotImg}
+              layout="responsive"
+              objectFit="conver"
+              alt="Headshot of Shanti Hedelin"
+              className="border rounded-xl mt-6"
+            />
+          </div>
+            <p className="pt-8">
+              Welcome to my page. I am a dedicated frontend developer with
+              significant experience in building high-performance and secure
+              websites, and I also have strong backend development skills. I
+              work well in teams and strive to deliver smart and intuitive code.
+              I'm detail-oriented and prioritize creating an optimal user
+              experience. My focus and dedication ensure that I always aim to
+              achieve the best possible results for every project I work on.
+            </p>
+            <button className="bg-transparent bg-white w-64 border p-4 shadow-xl rounded-3xl hover:cursor-pointer hover:w-96 transistion duration-300 ease-in-out mt-8">
+              View Resume
+            </button>
+          </div>
+
+        </div>
+      </div>
+
       <div className="flex flex-col items-center">
-        <h3 className="mt-24 flex justify-center text-5xl text-[#ea1179">I work with...</h3>
+        <h3 className="mt-24 flex justify-center text-5xl text-[#ea1179">
+          I work with...
+        </h3>
         <div className="scroll-container h-56 flex justify-center items-center">
           <div className="scroll-content space-x-8">
             <div className="h-40 w-40 rounded-full flex items-center justify-center skills">
